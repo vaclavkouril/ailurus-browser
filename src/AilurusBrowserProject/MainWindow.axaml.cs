c384bb629bc3fefdbd379456814f61085b8afed8
Vaclav Kouril <vaclav.kouril02@gmail.com>
2024-07-13 21:13:14 +0200

    Initialized project structure

---
+++ src/AilurusBrowserProject/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AilurusBrowserProject;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/AilurusBrowserProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using System;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.ReactiveUI;
+using Gecko;
+
+
+namespace AilurusBrowserProject;
+
+class Program
+{
+    class Program
+    {   
+        private static string FirefoxPath = "/usr/bin/firefox";
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Xpcom.Initialize(FirefoxPath);
+
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                         .UsePlatformDetect()
+                         .LogToTrace()
+                         .UseReactiveUI();
+    }
+}
